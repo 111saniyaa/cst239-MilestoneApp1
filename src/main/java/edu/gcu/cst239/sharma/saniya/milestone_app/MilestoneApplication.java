84f4a9079efb90f34e7d2921ee446b0f316e48b1
111saniyaa <saniya.sharma.2061@gmail.com>
2026-09-21 14:28:32 -0700

    first commit

---
+++ src/main/java/edu/gcu/cst239/sharma/saniya/milestone_app/MilestoneApplication.java
@@ -0,0 +1,12 @@
+package edu.gcu.cst239.sharma.saniya.milestone_app;
+
+
+    public class MilestoneApplication {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Milestone application starting..."); 
+    }
+}
+
+
